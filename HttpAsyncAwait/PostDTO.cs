791081550973770e4d394815b095ad6edad0bcc4
Oleg <serv_all@mail.ru>
2022-02-03 22:08:14 +0300

    Асинхронно запрашиваем с заданного url посты и записываем их в текстовый файл

---
+++ HttpAsyncAwait/PostDTO.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HttpAsyncAwait
+{
+    internal class PostDTO
+    {
+        public int userId { get; set; }
+        public int id { get; set; }
+        public string title { get; set; }
+        public string body { get; set; }
+
+        public List<string> ToStringList()
+        {
+            List<string> outList = new();
+            outList.Add(userId.ToString());
+            outList.Add(id.ToString());
+            outList.Add(title);
+            outList.Add(body);
+
+            return outList;
+        }
+
+    }
+}
