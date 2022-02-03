22589973dc81baea86afd1fcf4a98e1ca4fd472c
Oleg <serv_all@mail.ru>
2022-02-03 22:08:14 +0300

    Асинхронно запрашиваем с заданного url посты и записываем их в текстовый файл

--- HttpAsyncAwait/Program.cs
+++ HttpAsyncAwait/Program.cs
@@ -1,12 +1,47 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text.Json;
+using System.Threading.Tasks;
 
 namespace HttpAsyncAwait
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var tasksList = new List<Task<PostDTO>>();
+
+            for (int i = 4; i <= 13; i++)
+            {
+                tasksList.Add(GetPostByIdViaHTTPAsync(i));
+            }
+
+            await Task.WhenAll(tasksList);
+
+            foreach (var item in tasksList)
+            {
+                PostFileWriter.Write(item.Result);
+            }
+
         }
+
+        private static async Task<PostDTO> GetPostByIdViaHTTPAsync(int postId)
+        {
+            var uri = new Uri("https://jsonplaceholder.typicode.com/posts/" + postId);
+            var client = new HttpClient();
+            try
+            {
+                var response = await client.GetStringAsync(uri);
+                var post = JsonSerializer.Deserialize<PostDTO>(response);
+                Console.WriteLine($"Post id: {postId}");//для отладки и визуализации выполнения
+                return post;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
     }
 }
