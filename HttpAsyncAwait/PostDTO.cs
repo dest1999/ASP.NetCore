4205d930f4aea1513f801215f8a3d0d2912e2da3
Oleg <serv_all@mail.ru>
2022-02-07 14:31:23 +0300

    Исправлено именование переменных

--- HttpAsyncAwait/PostDTO.cs
+++ HttpAsyncAwait/PostDTO.cs
@@ -8,18 +8,18 @@ namespace HttpAsyncAwait
 {
     internal class PostDTO
     {
-        public int userId { get; set; }
-        public int id { get; set; }
-        public string title { get; set; }
-        public string body { get; set; }
+        public int UserId { get; set; }
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Body { get; set; }
 
         public List<string> ToStringList()
         {
             List<string> outList = new();
-            outList.Add(userId.ToString());
-            outList.Add(id.ToString());
-            outList.Add(title);
-            outList.Add(body);
+            outList.Add(UserId.ToString());
+            outList.Add(Id.ToString());
+            outList.Add(Title);
+            outList.Add(Body);
 
             return outList;
         }
